4afec587e710d2f6d31fa2ddbdd5a28fb4efacaa
Johan Lindkvist <johanlindkvist89@gmail.com>
2025-08-19 16:22:37 +0200

    feat: export prop function

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ export * from "./src/types.js";
 export * from "./src/utils/compose.js";
 export * from "./src/utils/curry.js";
 export * from "./src/utils/pipe.js";
+export * from "./src/utils/prop.js";
