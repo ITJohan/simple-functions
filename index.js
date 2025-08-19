0e8e7a571de71abf85641ecc15959df0210c9b0c
Johan Lindkvist <johanlindkvist89@gmail.com>
2025-08-19 13:44:17 +0200

    feat: add pipe function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export * from "./src/types.js";
 export * from "./src/utils/compose.js";
 export * from "./src/utils/curry.js";
+export * from "./src/utils/pipe.js";
--- src/utils/compose.js
+++ src/utils/compose.js
@@ -1,14 +1,15 @@
-/** 
+/**
  * @template A
  * @template Z
  * @param {[(x: any) => Z, ...Function[], (x: A) => any]} fns
  * @returns {(x: A) => Z}
  */
-const compose = (...fns) => (x) =>
-/** @type {Z} */
-(/** @type {unknown} */
-  (fns.reduceRight((x, fn) => fn(x), x)
+const compose = (...fns) => (x) => (
+  /** @type {Z} */ (
+    /** @type {unknown} */ (
+      fns.reduceRight((x, fn) => fn(x), x)
+    )
   )
 );
 
-export { compose }
+export { compose };
