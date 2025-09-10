0d5a4169143f942cc43cc6cd7292253cbf1c4fb6
Johan Lindkvist <johanlindkvist89@gmail.com>
2025-09-10 16:15:00 +0200

    Add run function

--- src/containers/task.js
+++ src/containers/task.js
@@ -10,6 +10,7 @@
  * @prop {<B>(fn: (x: A) => B) => Task<E, B>} map
  * @prop {<B>(fn: (x: A) => Task<E, B>) => Task<E, B>} chain
  * @prop {<B>(other: Task<E, (x: A) => B>) => Task<E, B>} ap
+ * @prop {() => Promise<A>} run
  */
 
 /**
@@ -36,6 +37,7 @@ const createTask = (computation) => ({
         .then(([fn, val]) => resolve(fn(val)))
         .catch(reject);
     }),
+  run: () => new Promise((resolve, reject) => computation(reject, resolve)),
 });
 
 /**
